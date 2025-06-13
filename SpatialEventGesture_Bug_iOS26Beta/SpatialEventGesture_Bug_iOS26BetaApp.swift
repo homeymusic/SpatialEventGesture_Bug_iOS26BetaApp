e6b25a3ee3df761510e83769662765f6d580f93b
Brian Mulloy <brian@landlessness.net>
2025-06-12 20:18:17 -0400

    Initial Commit

---
+++ SpatialEventGesture_Bug_iOS26Beta/SpatialEventGesture_Bug_iOS26BetaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpatialEventGesture_Bug_iOS26BetaApp.swift
+//  SpatialEventGesture_Bug_iOS26Beta
+//
+//  Created by Brian McAuliff Mulloy on 6/12/25.
+//
+
+import SwiftUI
+
+@main
+struct SpatialEventGesture_Bug_iOS26BetaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
